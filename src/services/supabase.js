dcb24c0e747876c522af1031593953441ec94c89
alexander lopez <iamlexlopez@gmail.com>
2026-01-07 10:41:13 +0800

    add gitiignore and environment variable

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://riiftzlywbcklgjotfax.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpaWZ0emx5d2Jja2xnam90ZmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNzgxMjcsImV4cCI6MjA4Mjc1NDEyN30.uxyWq6uGU7vx5wgp1Hnv9Q-IsAbdbX6HhAka_6gf3J0";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
